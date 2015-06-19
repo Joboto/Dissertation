7ac35caf60ce2da5cf0e1d5c3d2bbccf0c321220
Joboto <joeisamonkey@gmail.com>
2015-06-19 16:07:18 +0100

    Dumping excess baggage
    removing old/redundant classes
    Flying solo from here (not withstanding a revert)
    perhaps create new model and controller for text extraction/analysis
    part...

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,10 @@
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
-
 import org.joda.time.DateTime;
-
-import model.MyCalendar;
 import model.MyJodaCal;
 import view.JodaViewFrame;
-import view.ViewFrame;
 
 public class Main {
-	/**
-	 * Copied calendar main
-	 */
-	/*public static void main(String[] args) {
-        MyCalendar cal = new MyCalendar();
-		JFrame calendarFrame = new JFrame();
-        calendarFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        calendarFrame.setTitle("Calendar");
-        calendarFrame.setResizable(true);
-        calendarFrame.setPreferredSize(new Dimension(800, 500));
-        calendarFrame.add(new ViewFrame(cal));
-        calendarFrame.pack();
-        calendarFrame.setVisible(true);
-    }*/
-	/**
-	 * JodaCal main
-	 */
 	public static void main(String[] args) {
         MyJodaCal cal = new MyJodaCal();
         //add test events
@@ -43,11 +21,5 @@ public class Main {
         calendarFrame.pack();
         calendarFrame.setVisible(true);
     }
-	/**
-	 * orig MVC main, just for regex thingy
-	 */
-	/*public static void main(String[] args) {
-		RunMVC mainRunMVC = new RunMVC();
-	}*/
-
+	
 }
