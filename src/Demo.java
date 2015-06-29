2ce8f90f6d3e424f792071743772dcd59307d813
Joboto <joeisamonkey@gmail.com>
2015-06-29 16:15:39 +0100

    Playing with some ideas
    Was thinking of having a the DateTimeExtractor implement or extend from
    JodaTime...
    Will perhaps look more at this later.

--- src/Demo.java
+++ src/Demo.java
@@ -1,21 +1,28 @@
+
 import model.*;
 import controller.*;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Instant;
+import org.joda.time.Interval;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.Period;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Starting...");
 		DateTime now = new DateTime();
-		Month thisMonth = new Month(now);
+		Period hr = Period.hours(2);
+		DateTime then = now.plus(hr);
 		
-		String first = "Here it is: " + thisMonth.getDays().get(1).getDate().toString(DateTimeFormat.shortDate());
-		System.out.println(first);
+		Duration d = new Duration(now, then);
 		
-		System.out.println(Regexes.DAY.asString());
-
+		Interval i = new Interval(then, d);
+		
+		System.out.println(i.toPeriod().getHours());
+		System.out.println(i.getStart().hourOfDay().getAsText());
+		System.out.println(i.getEnd().hourOfDay().getAsText());
 	}
 
 }
