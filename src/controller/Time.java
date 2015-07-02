dedafdf439da07716e8855954c1f2e1d12d55d8d
Joboto <joeisamonkey@gmail.com>
2015-07-02 14:31:50 +0100

    Updating Time enum
    Wasn't happy with 24hour format with a space at the end, just had to
    update regex

--- src/controller/Time.java
+++ src/controller/Time.java
@@ -2,7 +2,7 @@ package controller;
 
 public enum Time {
 	
-	TWENTYFOURHOUR("[1-2]?[0-9]:[0-6][0-9][^a-zA-Z]", "H:m"),
+	TWENTYFOURHOUR("[1-2]?[0-9]:[0-6][0-9][^a-zA-Z ]", "H:m"),
 	TWELVEHOUR("1?[0-9]:[0-6][0-9][aApP][mM]", "h:ma"),
 	HOURONLY("1?[0-9][aApP][mM]", "ha");
 	
